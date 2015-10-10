6698498c2997b64327df81e028e3187b2bdd2a10
Siddharth Joshi <drumfiend21@gmail.com>
2015-10-10 16:23:02 -0400

    implement logging

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 var express = require("express");
 var app = express();
+var morgan = require("morgan");
+var chalk = require("chalk")
 
 
+var obj = {
+
+	'msg':'welcome to the JSON object page'
+}
 
 
+app.use(morgan(function (tokens, req, res) {
+  return chalk.blue(tokens.method(req, res))
+    + ' ' + chalk.green(tokens.url(req, res))
+    + ' ' + chalk.red(tokens['response-time'](req, res))
+}))
 
 app.get('/', function(req,res){
 	res.send("up and running");
 })
+
+app.get('/news', function(req,res){
+	res.send(obj.msg);
+})
+
 
 
 
