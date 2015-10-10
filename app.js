8e9381fdc1210bd0ba20100613016af3299baf5b
Siddharth Joshi <drumfiend21@gmail.com>
2015-10-10 18:33:07 -0400

    set up template in route

--- app.js
+++ app.js
@@ -2,12 +2,42 @@ var express = require("express");
 var app = express();
 var morgan = require("morgan");
 var chalk = require("chalk")
+var swig = require('swig');
 
+//set up templating
+app.engine('html', swig.renderFile);
+app.set('views', __dirname+'/views')
+app.set('view engine', 'html')
+
+//disable view cacheing for development
+swig.setDefaults({ cache: false });
 
 var obj = {
 
 	'msg':"this is the value, 'msg' is the key",
 	'success':"this life is going to be a success.  You will excel and God is with you.  No one wants you to fail.  You will succeed.  Just keep working hard.  You are loved and supported from above in your goals."
+}
+
+var data = {
+	title : 'I am Siddharth Joshi',
+	people : [
+		{
+			name : 'Evolved'
+		},
+		{
+			name : 'Wise'
+		},
+		{
+			name : 'Mowgli'
+		},
+		{
+			name : 'Inspiring'
+		},
+		{
+			name : 'Powerful'
+		}
+	]
+
 }
 
 
@@ -18,7 +48,7 @@ app.use(morgan(function (tokens, req, res) {
 }))
 
 app.get('/', function(req,res){
-	res.send("up and running");
+	res.render('index', data)
 })
 
 app.get('/news', function(req,res){
